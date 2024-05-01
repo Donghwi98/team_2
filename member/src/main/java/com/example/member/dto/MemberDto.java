67229bbedabefa7f0b976f27a2a0c8b60a3ff488
Kim DongHwi <hwi9158@gmail.com>
2024-05-01 12:13:27 +0900

    2.Dto 생성 완료

---
+++ member/src/main/java/com/example/member/dto/MemberDto.java
@@ -0,0 +1,14 @@
+package com.example.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+	int mno;
+	String id;
+	String pw;
+}
