659ae770ca4b8b93ae831a0695388191206b8699
Kim ye young <kimyyoung1018@gmail.com>
2024-05-01 14:43:37 +0900

    mapper 수정

--- member/src/main/java/com/example/member/controller/MyController.java
+++ member/src/main/java/com/example/member/controller/MyController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.member.dao.IMemberDao;
 
-import com.example.member.dao.IMemberDao;
 
 @Controller
 public class MyController {
